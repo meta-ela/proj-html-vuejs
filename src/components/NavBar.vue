e5324cb23a5880a6b20fa1b6211fad9a5758e59c
meta-ela <anxhelameta@live.it>
2022-07-01 16:11:15 +0200

    Adds TheHeader and NavBar components basic layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-
+        <!-- stampare i dati con v-for??? -->
+        <nav class="d-flex justify-content-between">
+            <div class="debug">
+                NEX GEN
+            </div>
+            <div>
+                <!-- stampare i dati con v-for??? -->
+                <ul class="d-flex gap-5 debug mb-0">
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>
+                        <a class="debug_border" href="javascript:void(0)">GET IN TOUCH</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
 </template>
 
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,42 @@
 <template>
     <div>
-        <NavBar></NavBar>
+        <!-- info-bar superiore fisso -->
+        <div class="info_bar_bg">
+            <div class="container">
+                <div class="d-flex justify-content-between">
+                    <div class="debug">
+                        Lorem ipsum dolor sit
+                    </div>
+                    <div>
+                        <!-- stampare i dati con v-for??? -->
+                        <ul class="d-flex gap-5 debug mb-0">
+                            <li>link</li>
+                            <li>link</li>
+                            <li>link</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- jumbotron image con dentro NavBar.vue -->
+        <div class="jumbotron_bg">
+            <div class="container">
+                <!-- nav bar con ciclo v-for -->
+                <NavBar></NavBar>
+                <div class="row row-cols-2">
+                    <div class="col debug mt-5">
+                        <small>LOREM LOREM</small>
+                        <P>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores quos corporis aliquid ex perspiciatis dolor odit iste voluptates debitis eius quo inventore exercitationem
+                        </P>
+                        <a class="debug_border" href="javascript:void(0)">GET IN TOUCH</a>
+                        <a class="debug_border" href="javascript:void(0)">READ MORE</a>
+                    </div>
+                    <div class="col"></div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 
